15929559df550a992568403286f182ad9b8f9b1d
Yuniru Yuni <yuniruyuni@gmail.com>
2022-11-03 00:59:52 +0900

    allow update via transfer host computer files

---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+CC_COMPUTER_PATH='C:\Users\yuniruyuni\curseforge\minecraft\Instances\YuniruCraft\saves\YuniruCraft\computercraft\computer'
+
+for FILE in `ls ${CC_COMPUTER_PATH}`
+do
+    FULLPATH="${CC_COMPUTER_PATH}\\${FILE}"
+    if [ -d $FULLPATH ]; then
+        echo "copying git content into ${FULLPATH}"
+        cp ./* "${FULLPATH}"
+    fi
+done
