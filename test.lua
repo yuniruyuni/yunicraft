527bf9cd4e344b40aea5a372fd5df97b7ecce5d8
Yuniru Yuni <yuniruyuni@gmail.com>
2023-03-10 22:42:50 +0900

    add result data output for file system

--- test.lua
+++ test.lua
@@ -18,12 +18,24 @@ local tests = concat(
     fs.find("bin/*_test.lua")
 )
 
+local logger = fs.open("test-result", "w")
+local buffer = ""
+
+function print(obj)
+    buffer = buffer .. obj
+    logger.writeLine(obj)
+end
+
 for i, f in ipairs(tests) do
     for mod in string.gmatch(f, "(bin/.+_test).lua") do
         local result = require(mod)
+        print("# test for " .. mod)
         print(result:resultText())
         print("-------")
         print(result:failuresText())
         print(result:succeededCount() .. " / " .. result:caseCount())
     end
 end
+
+-- textutils.pagedPrint(buffer)
+logger.close()
