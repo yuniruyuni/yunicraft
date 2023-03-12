e271f80a5782145325f7d7a7f2f1c517ad4aa68c
Yuniru Yuni <yuniruyuni@gmail.com>
2023-03-12 12:03:44 +0900

    change dig-tree feature for small auto tree firm

--- bin/dig-tree.lua
+++ bin/dig-tree.lua
@@ -1,18 +1,37 @@
-turtle.dig()
-turtle.forward()
-
-local moved = 0
 while true do
-    local found, inspected = turtle.inspectUp()
-    if (not found) or (inspected.name == "minecraft:birch_leaves") then
-        break
+    turtle.place()
+
+    -- wait tree grows
+    while true do
+        local found, inspected = turtle.inspect()
+        if (not found) or (inspected.name == "minecraft:birch_log") then
+            break
+        end
+
+        os.sleep(1)
     end
 
-    moved = moved + 1
-    turtle.digUp()
-    turtle.up()
-end
+    -- start dig tree
+    turtle.dig()
+    turtle.forward()
 
-for i = 0, moved do
-    turtle.down()
+    local moved = 0
+    while true do
+        local found, inspected = turtle.inspectUp()
+        if (not found) or (inspected.name == "minecraft:birch_leaves") then
+            break
+        end
+
+        moved = moved + 1
+        turtle.digUp()
+        turtle.up()
+    end
+
+    for i = 0, moved do
+        turtle.down()
+    end
+
+    turtle.back()
+
+    os.sleep(60 * 3)
 end
