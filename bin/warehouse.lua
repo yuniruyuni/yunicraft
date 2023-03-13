e79649de6272749febf4e3d4ee62ce532582285a
yuniruyuni <yuniruyuni@gmail.com>
2023-03-14 02:17:41 +0900

    feat: implement pull items from warehouse

--- bin/warehouse.lua
+++ bin/warehouse.lua
@@ -2,6 +2,8 @@ local Warehouse = require("model/warehouse")
 local Chest = require("model/chest")
 local Cell = require("model/cell")
 
+local choice = require("lib/choice")
+
 local modem = peripheral.find("modem")
 
 -- findSrcBarrel finds a barrel in connected computer network.
@@ -38,43 +40,87 @@ local CHEST_MAX_ITEM_COUNT = 54
 
 -- enumerate all storage chests
 local warehouse = Warehouse.default{}
+local transferTarget = ""
 
-local dstPrefixPattern = "minecraft:chest_"
+local dstPrefix = "minecraft:chest_"
 for _, dstName in ipairs(modem.getNamesRemote()) do
-    if string.match(dstName, dstPrefixPattern) then
+    if string.match(dstName, dstPrefix) then
         local dst = peripheral.wrap(dstName)
-        local items = dst.list()
+        if dst.size() == CHEST_MAX_ITEM_COUNT then
+            local items = dst.list()
 
-        -- 中身がないならemptyChestsとして登録
-        local chest = Chest.default{name = dstName}
-        for i = 1, CHEST_MAX_ITEM_COUNT do
-            local item = {}
-            if items[i] ~= nil then
-                item = items[i]
-            end
+            -- 中身がないならemptyChestsとして登録
+            local chest = Chest.default{name = dstName}
+            for i = 1, CHEST_MAX_ITEM_COUNT do
+                local item = {}
+                if items[i] ~= nil then
+                    item = items[i]
+                end
 
-            local cell = Cell.default(item)
-            table.insert(chest.cells, cell)
-        end
-
-        table.insert(warehouse.chests, chest)
-    end
-end
-
-while true do
-    for _, src in ipairs(srcs) do
-        for i, item in pairs(src.list()) do
-            if item ~= nil then
                 local cell = Cell.default(item)
-                -- print(textutils.serializeJSON(cell))
-                local chest = warehouse:findAvailableChestFor(cell)
-                -- print(textutils.serializeJSON(chest))
-                src.pushItems(chest.name, i, 64)
-                local pushed = warehouse:pushItem(cell)
-                -- print(textutils.serializeJSON(pushed))
-            else
-                print("item was nil")
+                table.insert(chest.cells, cell)
             end
+
+            table.insert(warehouse.chests, chest)
+        else
+            transferTarget = dstName
         end
     end
 end
+
+parallel.waitForAll(
+    -- thread for transfering from barrel to warehouse
+    function ()
+        while true do
+            for _, src in ipairs(srcs) do
+                for i, item in pairs(src.list()) do
+                    if item ~= nil then
+                        local cell = Cell.default(item)
+                        -- print(textutils.serializeJSON(cell))
+                        local chest = warehouse:findAvailableChestFor(cell)
+                        -- print(textutils.serializeJSON(chest))
+                        src.pushItems(chest.name, i, 64)
+                        local pushed = warehouse:pushItem(cell)
+                        -- print(textutils.serializeJSON(pushed))
+                    else
+                        print("item was nil")
+                    end
+                end
+            end
+        end
+    end,
+    -- thread for detecting transfer target chest.
+    function ()
+        local _, side = os.pullEvent("peripheral")
+        transferTarget = side
+        print("changed target: " .. transferTarget)
+    end,
+    -- thread for detecting new
+    function ()
+        while true do
+            local items = warehouse:listItems()
+
+            local indexedItems = {}
+            for _, item in pairs(items) do
+                -- cut empty cell
+                if item.name ~= "" then
+                    table.insert(indexedItems, item)
+                end
+            end
+
+            local selected = choice(indexedItems, function(item)
+                local sum = 0
+                for _, ref in ipairs(item.refs) do
+                    sum = sum + ref.count
+                end
+                return item.name .. " / " .. sum
+            end)
+
+            print("attempt to obtain " .. selected.name .. " from " .. transferTarget)
+            local target = peripheral.wrap(transferTarget)
+            for _, ref in ipairs(selected.refs) do
+                target.pullItems(ref.chestName, ref.cellIndex)
+            end
+        end
+    end
+)
